b418c4b31ee66a9d5074239f3b35e399a4267b41
joaquin mora <rodolfomorag7@gmail.com>
2026-08-19 21:03:34 -0600

    pages
    i added lines to start the app in pages also i am testing the buttons

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import customtkinter as ctk
+
 
 mainurl = "https://es.wikipedia.org"
 
@@ -8,5 +10,36 @@ headers = {
 }
 
 respuesta = requests.get(mainurl,headers=headers)
+soup = BeautifulSoup(respuesta.text, "lxml")
+titulo = soup.find("h1", id="firstHeading").text
 
-print(respuesta)
+
+
+
+#esto es para la pagina principal xdxdxd
+
+
+mainpage = ctk.CTk()
+mainpage.title(titulo)
+mainpage.geometry("1000x600")
+titulomp = ctk.CTkLabel(mainpage,text="WikiWrapTool", font=("Arial", 50), anchor="e")
+titulomp.pack(pady=40)
+mainpage.configure(fg_color="#1F1F2E")
+
+
+
+def boton_buscar():
+    pass
+
+
+boton = ctk.CTkButton(mainpage, text="start to scrap ou yeahh", command=boton_buscar, font=("Arial", 20, "bold"))
+boton.pack(pady=80)
+
+
+insert
+
+
+
+
+
+mainpage.mainloop()
