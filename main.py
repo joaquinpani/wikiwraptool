1671f817293fb091570e7d8d22258edf791097a1
joaquin mora <rodolfomorag7@gmail.com>
2026-08-19 20:23:16 -0600

    the start
    i am testing what i can do in  this moment i dont know what i  need to do like what it just print 200?? also i added the request import and use it (i see i can get the things from the page with this)

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("inicio")dd
+import requests
+from bs4 import BeautifulSoup
+
+mainurl = "https://es.wikipedia.org"
+
+headers = {
+"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+respuesta = requests.get(mainurl,headers=headers)
+
+print(respuesta)
